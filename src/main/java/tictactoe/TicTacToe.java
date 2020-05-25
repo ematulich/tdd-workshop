9f9214cb7878e76893713ff9354c1c5e9aa66797
ematulich <emanuel.matulich@mercadolibre.com>
2020-05-25 13:39:59 -0300

    the most simple test

--- src/main/java/tictactoe/TicTacToe.java
+++ src/main/java/tictactoe/TicTacToe.java
@@ -3,8 +3,6 @@ package tictactoe;
 public class TicTacToe {
     private final static String EMPTY_POSITION = "-";
     private final static String X_PLAYER = "X";
-    private final static String O_PLAYER = "O";
-    private String currentPlayer;
     private final String[][] board = new String[][]{
             {EMPTY_POSITION,EMPTY_POSITION,EMPTY_POSITION},
             {EMPTY_POSITION,EMPTY_POSITION,EMPTY_POSITION},
@@ -30,7 +28,7 @@ public class TicTacToe {
     }
 
     public String nextPlayer(){
-        return X_PLAYER.equals(currentPlayer)? O_PLAYER: X_PLAYER;
+        return X_PLAYER;
     }
 
 }
