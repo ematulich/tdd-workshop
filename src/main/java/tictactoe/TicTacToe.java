0d77f1cf34a8c9142a8c2cbed0e571f3dc0b1afd
ematulich <emanuel.matulich@mercadolibre.com>
2020-05-24 21:58:11 -0300

    First Test - X outside board validation

--- src/main/java/tictactoe/TicTacToe.java
+++ src/main/java/tictactoe/TicTacToe.java
@@ -1,4 +1,12 @@
 package tictactoe;
 
 public class TicTacToe {
+
+
+    public void play(int x, int y) throws RuntimeException{
+       if ( x < 1 || x > 3){
+           throw new RuntimeException("Position selected outside the board");
+       }
+    }
+
 }
--- src/test/java/tictactoe/TicTacToeSpec.java
+++ src/test/java/tictactoe/TicTacToeSpec.java
@@ -1,4 +1,30 @@
 package tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private TicTacToe game;
+
+    @Before
+    public final void beforeSetUp(){
+        game = new TicTacToe();
+    }
+
+
+    @Test
+    public void whenXAxisIsOutsideBoardThenRuntimeException(){
+        exception.expect(RuntimeException.class);
+        game.play(4, 2);
+    }
+
+
+
+
 }
