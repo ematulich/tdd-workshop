1b36789d0f176f68935ddbe1c0a7fbe017ee663e
ematulich <emanuel.matulich@mercadolibre.com>
2020-05-29 14:36:11 -0300

    add utils

--- src/test/java/tictactoe/TicTacToeSpec.java
+++ src/test/java/tictactoe/TicTacToeSpec.java
@@ -1,4 +1,13 @@
 package tictactoe;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
 }
